c02116aaf5f743b90083a57f9208ca3d13e22501
Dark6767 <48690111+Dark6767@users.noreply.github.com>
2019-03-27 20:31:48 +0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -2,5 +2,8 @@ def sorting(string):
     words = string.split()
     result = sorted(words)
     return result
-
-print(sorting(input('asd')))
+def main():
+    print(sorting(input('Введите предложение')))
+   
+if __name__ == '__main__':
+    main()
